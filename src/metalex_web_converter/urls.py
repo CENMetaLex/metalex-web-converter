1c4585b1863b390d8d33cf4731e0afda2fad6ce2
Rinke Hoekstra <rjhoekstr@gmail.com>
2011-04-20 21:25:00 +0200

    Changed redirects

--- src/metalex_web_converter/urls.py
+++ src/metalex_web_converter/urls.py
@@ -1,12 +1,14 @@
 from django.conf.urls.defaults import patterns, include, url
-from metalex_web_converter.views import rdf_data, xml_data, html_page
+from metalex_web_converter.views import rdf_data, xml_data, html_data, negotiate, redirect
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
 urlpatterns = patterns('',
-    ('^BWB(\w\d+)/(.*?)(\d\d\d\d-\d\d-\d\d)/data.xml$', xml_data),
-    ('^BWB(\w\d+)/(.*?)(\d\d\d\d-\d\d-\d\d)/data.rdf$', rdf_data),
-    ('^BWB(\w\d+)/(.*)/$', html_page)
+    ('^doc/BWB(\w\d+)/(.*?)(\d\d\d\d-\d\d-\d\d)/data.xml$', xml_data),
+    ('^doc/BWB(\w\d+)/(.*?)(\d\d\d\d-\d\d-\d\d)/data.rdf$', rdf_data),
+    ('^doc/BWB(\w\d+)/(.*?)(\d\d\d\d-\d\d-\d\d)/data.html$', html_data),
+    ('^doc/BWB(\w\d+)/(.*)/$', negotiate)
+    ('^id/BWB(\w\d+)/(.*)/$', redirect)
 )
--- src/metalex_web_converter/views.py
+++ src/metalex_web_converter/views.py
@@ -10,7 +10,7 @@ from SPARQLWrapper import SPARQLWrapper, JSON
 def xml_data(request, bwbnr, doc, version):
     if check_available(bwbnr, doc, version) :
         xml_response = HttpResponse('')
-        xml_response.status_code = '303'
+        xml_response.status_code = '302 Found'
         xml_response['Location'] = 'http://u017101.jur.uva.nl/files/BWB{0}_{1}_ml.xml'.format(bwbnr,version)
             
         return xml_response
@@ -20,26 +20,54 @@ def xml_data(request, bwbnr, doc, version):
 def rdf_data(request, bwbnr, doc, version):
     if check_available(bwbnr, doc, version) :
         rdf_response = HttpResponse('')
-        rdf_response.status_code = '303'
+        rdf_response.status_code = '302 Found'
         rdf_response['Location'] = 'http://u017101.jur.uva.nl/files/BWB{0}_{1}.n3'.format(bwbnr,version)
             
         return rdf_response   
     else :
         return HttpResponse("Document has not been converted yet ...")
 
-def html_page(request, bwbnr, doc):
+def html_data(request, bwbnr, doc, version):
+    if check_available(bwbnr, doc, version) :
+        rdf_response = HttpResponse('')
+        rdf_response.status_code = '302 Found'
+        rdf_response['Location'] = 'http://u017101.jur.uva.nl:3020/browse/list_resource?r=http://doc.metalex.eu/BWB{0}/{1}/{2}'.format(bwbnr,doc,version)
+            
+        return rdf_response   
+    else :
+        return HttpResponse("Document has not been converted yet ...")
+
+
+def negotiate(request, bwbnr, doc):
     accept_header = request.META['HTTP_ACCEPT']
     
     if accept_header.find('html') != -1:
         html_response = HttpResponse('')
-        html_response.status_code = '303'
-        html_response['Location'] = 'http://u017101.jur.uva.nl:3020/browse/list_resource?r=http://doc.metalex.eu/BWB{0}/{1}'.format(bwbnr,doc)
+        html_response.status_code = '302 Found'
+        html_response['Location'] = 'http://doc.metalex.eu/doc/BWB{0}/{1}/data.html'.format(bwbnr,doc)
         
         return html_response
-    elif accept_header.startswith('application/rdf+xml') :        
-        return HttpResponse("No RDF here")
+    elif accept_header.startswith('application/rdf+xml' or accept_header.startswith('application/x-turtle') or accept_header.startswith('text/rdf+n3')) :        
+        rdf_response = HttpResponse('')
+        rdf_response.status_code = '302 Found'
+        rdf_response['Location'] = 'http://doc.metalex.eu/doc/BWB{0}/{1}/data.rdf'.format(bwbnr,doc)
+        
+        return rdf_response
+    elif accept_header.startswith('application/xml') or accept_header.startswith('text/xml'):        
+        xml_response = HttpResponse('')
+        xml_response.status_code = '302 Found'
+        xml_response['Location'] = 'http://doc.metalex.eu/doc/BWB{0}/{1}/data.xml'.format(bwbnr,doc)
+        
+        return xml_response
     else :
         return HttpResponse("Accept header is: {0}".format(accept_header))
+
+def redirect(request, bwbnr, doc):
+    redir_response = HttpResponse('')
+    redir_response.status_code = '303'
+    redir_response['Location'] = 'http://doc.metalex.eu/doc/BWB{0}/{1}'.format(bwbnr,doc)
+    
+    return redir_response
 
 
 def check_available(bwbnr, doc, version):
