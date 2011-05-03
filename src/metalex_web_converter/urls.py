ba36fe654c4e25015410ba76d5f84c89337f7eb9
Rinke Hoekstra <rjhoekstr@gmail.com>
2011-05-03 14:41:17 +0200

    huh

--- src/metalex_web_converter/urls.py
+++ src/metalex_web_converter/urls.py
@@ -1,21 +1,22 @@
 from django.conf.urls.defaults import patterns
-from metalex_web_converter.views import redirect_to_latest, rdf_expression_data, xml_expression_data, no_xml_work_data, html_expression_data, rdf_work_data, html_work_data, negotiate, convert, redirect
+from metalex_web_converter.views import index, redirect_to_latest, rdf_expression_data, xml_expression_data, no_xml_work_data, html_expression_data, rdf_work_data, html_work_data, negotiate, convert, redirect
 
 # Uncomment the next two lines to enable the admin:
 # from django.contrib import admin
 # admin.autodiscover()
 
 urlpatterns = patterns('',
-    ('^doc/BWB(\w\d+)(.*?)(\d\d\d\d-\d\d-\d\d)/data.xml$', xml_expression_data),
-    ('^doc/BWB(\w\d+)(.*?)/data.xml$', no_xml_work_data),
-    ('^doc/BWB(\w\d+)(.*?)(\d\d\d\d-\d\d-\d\d)/data.rdf$', rdf_expression_data),
-    ('^doc/BWB(\w\d+)(.*?)/data.rdf$', rdf_work_data),
-    ('^doc/BWB(\w\d+)(.*?)(\d\d\d\d-\d\d-\d\d)/data.html$', html_expression_data),
-    ('^doc/BWB(\w\d+)(.*?)/data.html$', html_work_data),
-    ('^doc/BWB(\w\d+)(.*?)$', negotiate),
-    ('^id/BWB(\w\d+)(.*)/latest', redirect_to_latest),
-    ('^id/BWB(\w\d+)(.*)$', redirect),
-    ('^convert/(.+)$', convert)
+    ('^doc/(?P<bwbid>BWB\w\d+)(?P<path>.*?)(?P<version>\d\d\d\d-\d\d-\d\d)/data.xml$', xml_expression_data),
+    ('^doc/(?P<bwbid>BWB\w\d+)(?P<path>.*?)/data.xml$', no_xml_work_data),
+    ('^doc/(?P<bwbid>BWB\w\d+)(?P<path>.*?)(?P<version>\d\d\d\d-\d\d-\d\d)/data.rdf$', rdf_expression_data),
+    ('^doc/(?P<bwbid>BWB\w\d+)(?P<path>.*?)/data.rdf$', rdf_work_data),
+    ('^doc/(?P<bwbid>BWB\w\d+)(?P<path>.*?)(?P<version>\d\d\d\d-\d\d-\d\d)/data.html$', html_expression_data),
+    ('^doc/(?P<bwbid>BWB\w\d+)(?P<path>.*?)/data.html$', html_work_data),
+    ('^doc/(?P<bwbid>BWB\w\d+)(?P<path>.*?)$', negotiate),
+    ('^id/(?P<bwbid>BWB\w\d+)(?P<path>.*)/latest', redirect_to_latest),
+    ('^id/(?P<bwbid>BWB\w\d+)(?P<path>.*)$', redirect),
+    ('^convert/(.+)$', convert),
+    ('^$', index)
 )
 
 
